265a35447a1f0b79fe931296197bded9850bdfe8
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2024-03-19 20:19:42 +0530

    stage_03_final

--- Prank_website/script.js
+++ Prank_website/script.js
@@ -59,8 +59,69 @@ var userCollection = {
   105523861058: "syed yadullah",
   105523861059: "ubaid ur rahman",
   105523861060: "wajeeda zainub",
+};
 
-
+const userCollectionNO = {
+  105523861001: 9550322451,
+  105523861002: 9885346333,
+  105523861003: 9618812915,
+  105523861004: 9490022615,
+  105523861005: 8688838992,
+  105523861006: 9966183474,
+  105523861007: 9966188874,
+  105523861008: 6304782956,
+  105523861009: 9290019384,
+  105523861010: 8686588321,
+  105523861011: 9347723405,
+  105523861012: 9966366774,
+  105523861013: 7997868955,
+  105523861014: 6304782956,
+  105523861015: 8688769725,
+  105523861016: 9705323167,
+  105523861017: 9182487340,
+  105523861018: 8465884220,
+  105523861019: "Not found",
+  105523861020: 7780263739,
+  105523861021: 9550108316,
+  105523861022: 7036437271,
+  105523861023: 6305160294,
+  105523861024: 7331152010,
+  105523861025: 8185890113,
+  105523861026: 8886506845,
+  105523861027: 7799592887,
+  105523861028: 8247449687,
+  105523861029: 9985986557,
+  105523861030: 9290553307,
+  105523861031: 6301825270,
+  105523861032: 6301265270,
+  105523861033: 9603166806,
+  105523861034: 6301062045,
+  105523861035: 9989369764,
+  105523861036: 9491328598,
+  105523861037: 8790905754,
+  105523861038: 8688149792,
+  105523861039: 9440402709,
+  105523861040: "NOT FOUND",
+  105523861041: 8008163634,
+  105523861042: 7995804647,
+  105523861043: 9949558375,
+  105523861044: 9291623470,
+  105523861045: 9420250303,
+  105523861046: 7901028613,
+  105523861047: 9398850021,
+  105523861048: 9603990189,
+  105523861049: 8008779436,
+  105523861050: 8978612903,
+  105523861051: 8008841134,
+  105523861052: 9618678942,
+  105523861053: 9618246642,
+  105523861054: 9032631929,
+  105523861055: 7036523158,
+  105523861056: 7993904051,
+  105523861057: 9963342339,
+  105523861058: "Not found",
+  105523861059: "Not found",
+  105523861060: 9963344739,
 };
 
 // predefined data
@@ -131,14 +192,16 @@ const gpaValue = document.getElementById("gpa");
 // getting data through html
 const course_details = "BCA(CBCS)";
 const medium_details = "ENGLISH";
-function inputTag() {
-  let classChange = document.getElementById('studentData');
-  classChange.classList.remove("hiddenStudentData");
-  classChange.classList.add("studentData");
+const sigma = document.getElementById("sigma1");
 
+function inputTag() {
+  let classChange = document.getElementById("studentData");
+  classChange.classList.add("studentData");
+  classChange.classList.remove("hiddenStudentData");
 
   const gender_male = "MALE";
   const gender_Female = "Female";
+  const gender_third = "GAY";
   const gender = document.getElementById("gender");
   const nameOutput = document.getElementById("NameOutput");
   const searchInput = document.getElementById("inputHall");
@@ -146,6 +209,8 @@ function inputTag() {
   const HallTicketNumber = searchInputValue;
   const getData = userCollection[HallTicketNumber];
   const getDataFinal = getData.toUpperCase();
+  const phoneNo = document.getElementById("phoneno");
+  phoneNo.textContent = userCollectionNO[HallTicketNumber];
 
   // course
 
@@ -155,7 +220,28 @@ function inputTag() {
   //   GENDER
 
   nameOutput.textContent = getDataFinal;
-  gender.textContent = gender_male;
+
+  if (
+    HallTicketNumber == "105523861022" ||
+    HallTicketNumber == "105523861011" ||
+    HallTicketNumber == "105523861029"
+  ) {
+    gender.textContent = gender_third;
+  } else if (
+    HallTicketNumber == "105523861010" ||
+    HallTicketNumber == "105523861009" ||
+    HallTicketNumber == "105523861042" ||
+    HallTicketNumber == "105523861051" ||
+    HallTicketNumber == "105523861060" ||
+    HallTicketNumber == "122623861017"
+  ) {
+    gender.textContent = gender_Female;
+  } else {
+    gender.textContent = gender_male;
+  }
+
+  // nameOutput.textContent = getDataFinal;
+  // gender.textContent = gender_male;
 
   //   printing hallticket number
 
